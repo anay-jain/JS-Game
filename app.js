83af3d7f9fdc29f83ae01b3ac688e609dba0e23a
anay-jain <anayjain98@gmail.com>
2020-05-19 14:24:21 +0530

    added movable objects

---
+++ app.js
@@ -0,0 +1,78 @@
+
+canvas = document.getElementById("canva1");
+console.log(canvas);
+
+w = 700;
+h= 400;
+// setting width
+canvas.height = h;
+canvas.width =w;
+
+pen = canvas.getContext('2d');
+console.log(pen);
+function init(){
+    canvas = document.getElementById("canva1");
+    console.log(canvas);
+    
+    w = 700;
+    h= 400;
+    // setting width
+    canvas.height = h;
+    canvas.width =w;
+    
+    pen = canvas.getContext('2d');
+    console.log(pen);
+    // creating JSON objects
+    e1 = {
+        x:10,
+        y:10,
+        wbird : 60,
+        hbird : 60,
+        speed : 10,
+    };
+    e2 = {
+        x:10,
+        y:10,
+        wbird : 60,
+        hbird : 60,
+        speed : 10,
+    };
+    e3 = {
+        x:10,
+        y:10,
+        wbird : 60,
+        hbird : 60,
+        speed : 10,
+    };
+    virus = [e1,e2,e3];
+  virus_image = new Image;
+  virus_image.src="Assets/v1.png"
+
+}
+function draw(){
+    pen.clearRect(0,0,w,h);
+    // pen.fillRect(bird.x,bird.y,bird.wbird,bird.hbird);
+    pen.drawImage(virus_image,bird.x,bird.y,bird.wbird,bird.hbird)
+    // pen.drawImage(virus_image,bird.x+300,bird.y+200,bird.wbird,bird.hbird)
+    // pen.drawImage(virus_image,bird.x+400,bird.y+40,bird.wbird,bird.hbird)
+}
+function update(){
+    bird.y+=bird.speed;
+    if(bird.y>h || bird.y<=0){
+        bird.speed*=-1;
+    }
+    // if(bird.y<=0){
+    //     bird.speed*=-1;
+    //     bird.x+=10;
+    // }
+}
+function gameloop(){
+    draw();
+    update();
+ console.log("mei bhi aagya bro");
+}
+init();
+setInterval(gameloop,100);
+
+
+
